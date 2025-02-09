eb926855669181dc943bfaba659327f49de7c368
Mustafa <gameacedamy@gmail.com>
2025-02-09 15:32:17 +0300

    cleaned code

--- resources-dlc3/scripts/items/Schizophrenia.lua
+++ resources-dlc3/scripts/items/Schizophrenia.lua
@@ -4,79 +4,7 @@ local hallucinations = {} -- Table to track affected tears
 local vulnerableEnemies = {}
 local player = Isaac.GetPlayer(0)
 
--- -- Disable enemy collisions with a random chance
--- function Schizophrenia:GetTearParent(entity)
---     local activeEnemyCtr = 0
---     for _, entity in ipairs(Isaac.GetRoomEntities()) do
---         if entity:IsActiveEnemy(false) then
---             activeEnemyCtr = activeEnemyCtr + 1
---         end
---     end
---     for _, entity in ipairs(Isaac.GetRoomEntities()) do
---         if entity.Type == EntityType.ENTITY_PROJECTILE then
---             local proj = entity:ToProjectile()
---             local spawner = proj.SpawnerEntity
 
-
-
---             if spawner then
---                 for _, value in ipairs(hallucinations) do
---                     if GetPtrHash(spawner) == GetPtrHash(value) then
---                         print("hello")
---                         proj:AddProjectileFlags(ProjectileFlags.CANT_HIT_PLAYER)
-
---                         break
---                     end
---                 end
---             end
---         elseif entity:IsActiveEnemy(false) and entity.SpawnerEntity ~= nil then
---             local exists = false
---             for _, value in ipairs(hallucinations) do
---                 if GetPtrHash(entity) == GetPtrHash(value) then
---                     exists = true
-
---                     break
---                 end
---             end
---             local parentIsHallucination = false
-
---             for _, value in ipairs(hallucinations) do
---                 if GetPtrHash(entity.SpawnerEntity) == GetPtrHash(value) then
---                     parentIsHallucination = true
-
---                     break
---                 end
---             end
---             if not exists and parentIsHallucination then
---                 invinciCtr = invinciCtr + 1
---                 table.insert(hallucinations, entity)
---             end
---         end
---     end
---     local roomEntities = Isaac.GetRoomEntities()
-
---     -- Filter to get only vulnerable enemies
---     local vulnerableEnemies = {}
---     for _, entity in ipairs(roomEntities) do
---         if entity:IsVulnerableEnemy() then
---             table.insert(vulnerableEnemies, entity)
---         end
---     end
-
---     -- If all vulnerable enemies in the room are hallucinations, remove all enemies
---     if activeEnemyCtr <= #hallucinations + invinciCtr then
---         Schizophrenia:FadeOut(hallucinations)
---         -- for _, entity in ipairs(hallucinations) do
---         --     -- entity:Kill() -- Kill the enemy
-
---         -- end
---     end
-
---     -- Print the count of hallucinations and vulnerable enemies
---     print(activeEnemyCtr, #hallucinations, invinciCtr)
--- end
-
--- Reset enemy and tear collisions when transitioning to a new room
 function Schizophrenia:OnNpcInit(entity)
     if player:HasCollectible(Schizophrenia.ID) then
         if entity:IsVulnerableEnemy() and entity.Type ~= EntityType.ENTITY_FIREPLACE and entity.Type ~= EntityType.ENTITY_BOMB then
