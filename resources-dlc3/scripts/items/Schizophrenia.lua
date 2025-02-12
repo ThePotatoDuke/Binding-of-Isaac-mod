bf667d6e1028eb46230e233944462a3f369e8790
Mustafa <gameacedamy@gmail.com>
2025-02-12 23:30:11 +0300

    removed pring

--- resources-dlc3/scripts/items/Schizophrenia.lua
+++ resources-dlc3/scripts/items/Schizophrenia.lua
@@ -107,7 +107,6 @@ function Schizophrenia:OnEntityKill(entity)
 end
 
 function Schizophrenia:OnUpdate()
-    print("Schizophrenia OnUpdate running")
     if #vulnerableEnemies == 0 and not fading then
         fading = true
     end
